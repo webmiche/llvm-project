febcfc10bf0d531c75692b7b3dc325af1afc4bf6
Michel Weber <michel.weber@inf.ethz.ch>
2025-01-31 15:07:05 +0100

    use fewer access calls

--- CFFunctionInstrumentation/parse_found_funcs.py
+++ CFFunctionInstrumentation/parse_found_funcs.py
@@ -3,10 +3,10 @@ from argparse import ArgumentParser
 from utils import demangle_function_name
 
 def unique_funcs(funcs_per_module):
-    unique_funcs = set()
+    unique_functions = set()
     for module, funcs in funcs_per_module.items():
-        unique_funcs.update(funcs)
-    return unique_funcs
+        unique_functions.update(funcs)
+    return unique_functions
 
 
 def parse_called_funcs(filename):
@@ -35,22 +35,72 @@ def get_unique_funcs(filename):
     funcs_per_module = parse_called_funcs(filename)
     return unique_funcs(funcs_per_module)
 
+def get_stored_funcs_dict(filename):
+    file = open(filename, "r")
+    funcs_per_module = eval(file.read())
+
+    return funcs_per_module
+
+def print_stats(funcs_per_module):
+
+    unique_functions = unique_funcs(funcs_per_module)
+
+    print(f'Found {len(funcs_per_module)} modules')
+
+    print(f'Found {len(unique_functions)} unique functions')
+
+def build_folder_hierarchy(modules):
+    hierarchy = {}
+    for module in modules:
+        module = module.removeprefix('/home/webmiche/questions/llvm-project/')
+        parts = module.split('/')
+        current = hierarchy
+        for part in parts:
+            if part not in current:
+                current[part] = {}
+            current = current[part]
+    return hierarchy
+
+def print_hierarchy(hierarchy):
+    for outer_most, inner in hierarchy.items():
+        print(outer_most)
+        for inner_most, inner_inner in inner.items():
+            print(f'  {inner_most}')
+            for inner_inner_most in inner_inner:
+                print(f'    {inner_inner_most}')
+                for inner_inner_inner in inner_inner[inner_inner_most]:
+                    print(f'      {inner_inner_inner}')
+                    for inner_inner_inner_inner in inner_inner[inner_inner_most][inner_inner_inner]:
+                        print(f'        {inner_inner_inner_inner}')
+                print()
+            print()
+        print()
+
+
+
 if __name__ == '__main__':
     # pass the filename as an argument
     parser = ArgumentParser()
     parser.add_argument('filename')
     args = parser.parse_args()
     filename = args.filename
+    funcs_per_module = get_stored_funcs_dict(filename)
 
-    unique_funcs = get_unique_funcs(filename)
 
-    print(f'Found {len(unique_funcs)} unique functions in {filename}:')
+    hierarchy = build_folder_hierarchy(funcs_per_module.keys())
 
-    funcs_per_module = parse_called_funcs(filename)
+    print_hierarchy(hierarchy)
 
-    print(f'Found {len(funcs_per_module)} modules in {filename}:')
+    # add up all the functions in the TableGen folder
+    functions_in_tablegen = {}
     for module, funcs in funcs_per_module.items():
-        print(f'{module}: {len(funcs)} functions')
+        if module.startswith('/home/webmiche/questions/llvm-project/llvm/utils/TableGen'):
+            functions_in_tablegen[module] = funcs
 
-    f = open("funcs_per_module.txt", "w")
-    f.write(str(funcs_per_module))
+    unique_functions = unique_funcs(functions_in_tablegen)
+    total_functions = len(unique_functions)
+
+
+    print_stats(funcs_per_module)
+
+    print(f'Functions in TableGen: {total_functions}')
