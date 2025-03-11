cc9bc38a29adec2d784094155c6e819a0b7c6098
Michel Weber <michel.weber@inf.ethz.ch>
2025-03-11 12:00:16 +0100

    change run2

--- benchmark_example/run2.sh
+++ benchmark_example/run2.sh
@@ -2,5 +2,6 @@
 
 echo "Clang"
 
+#../build_instr/bin/clang main2.c foo_precise2.c foo_relaxed2.c -O3 -march=native -o out.o
 clang main2.c foo_precise2.c foo_relaxed2.c -O3 -march=native -o out.o
 ./out.o
